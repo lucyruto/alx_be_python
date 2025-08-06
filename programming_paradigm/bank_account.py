4d4b09423009dcac93f79f36740c31500d8638f9
lucyruto <lucyruto.lr@gmail.com>
2025-08-06 13:56:15 +0300

    Adding bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,5 +13,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current account balance: {self.account_balance}")
+        print(f"Current Balance: {self.account_balance}")
 
